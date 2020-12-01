a2fcfa8e21c9a0d6a79ec63c7dc09787dbb9e064
VeggieOrz <lwh_233@163.com>
2020-12-01 23:15:07 +0800

    初始化项目

---
+++ life/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  life
+//
+//  Created by liuweihong on 2020/11/28.
+//  Copyright © 2020 veggie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (nonatomic, strong) UIWindow *window;
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ life/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  life
+//
+//  Created by liuweihong on 2020/11/28.
+//  Copyright © 2020 veggie. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+
+@end
