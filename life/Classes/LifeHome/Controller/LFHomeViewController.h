e0dc3097b8d8b8ce29e6713af5d60c14cd64fdde
VeggieOrz <lwh_233@163.com>
2021-01-31 12:26:37 +0800

    添加主页文件

---
+++ life/Classes/LifeHome/Controller/LFHomeViewController.h
@@ -0,0 +1,17 @@
+//
+//  LFHomeViewController.h
+//  life
+//
+//  Created by liuweihong on 2020/12/5.
+//  Copyright © 2020 veggie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LFHomeViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ life/Classes/LifeHome/Controller/LFHomeViewController.m
@@ -0,0 +1,22 @@
+//
+//  LFHomeViewController.m
+//  life
+//
+//  Created by liuweihong on 2020/12/5.
+//  Copyright © 2020 veggie. All rights reserved.
+//
+
+#import "LFHomeViewController.h"
+
+@interface LFHomeViewController ()
+
+@end
+
+@implementation LFHomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+@end
