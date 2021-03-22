11005b38779062602cb4c5288f8636c25e1b8789
VeggieOrz <lwh_233@163.com>
2021-03-22 10:45:10 +0800

    添加日记详情页Header

---
+++ life/Classes/LifeEdit/View/LFDiaryDetailHeaderView.h
@@ -0,0 +1,17 @@
+//
+//  LFDiaryDetailHeaderView.h
+//  life
+//
+//  Created by liuweihong on 2021/3/20.
+//  Copyright © 2021 veggie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LFDiaryDetailHeaderView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ life/Classes/LifeEdit/View/LFDiaryDetailHeaderView.m
@@ -0,0 +1,21 @@
+//
+//  LFDiaryDetailHeaderView.m
+//  life
+//
+//  Created by liuweihong on 2021/3/20.
+//  Copyright © 2021 veggie. All rights reserved.
+//
+
+#import "LFDiaryDetailHeaderView.h"
+
+@implementation LFDiaryDetailHeaderView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
