29e113ace5401e82846e937b4fa8e836ae5b643c
VeggieOrz <lwh_233@163.com>
2021-03-26 15:12:00 +0800

    添加素材，创建个人主页

---
+++ life/Classes/LifeProfile/Controller/LFUserProfileViewController.h
@@ -0,0 +1,17 @@
+//
+//  LFUserProfileViewController.h
+//  life
+//
+//  Created by liuweihong on 2021/3/26.
+//  Copyright © 2021 veggie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LFUserProfileViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
