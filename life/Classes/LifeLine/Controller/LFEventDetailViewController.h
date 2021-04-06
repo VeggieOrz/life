8eaa09d05ba4dd2a8267fd5b0b45092eb0b66acf
VeggieOrz <lwh_233@163.com>
2021-04-06 17:22:03 +0800

    添加时间轴页面文件

---
+++ life/Classes/LifeLine/Controller/LFEventDetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  LFEventDetailViewController.h
+//  life
+//
+//  Created by liuweihong on 2021/4/6.
+//  Copyright © 2021 veggie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LFEventDetailViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ life/Classes/LifeLine/Controller/LFEventEditViewController.h
@@ -0,0 +1,17 @@
+//
+//  LFEventEditViewController.h
+//  life
+//
+//  Created by liuweihong on 2021/4/6.
+//  Copyright © 2021 veggie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LFEventEditViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
