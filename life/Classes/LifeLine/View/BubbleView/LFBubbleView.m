50dea9a8dbcc01207161c923dfcdecf0f13ad554
VeggieOrz <lwh_233@163.com>
2021-04-29 10:10:00 +0800

    气泡去掉边框添加阴影

--- life/Classes/LifeLine/View/BubbleView/LFBubbleView.m
+++ life/Classes/LifeLine/View/BubbleView/LFBubbleView.m
@@ -20,6 +20,11 @@
 - (instancetype)initWithFrame:(CGRect)frame {
     if (self = [super initWithFrame:frame]) {
         [self.layer addSublayer:self.shapeLayer];
+        // 添加阴影
+        self.layer.shadowColor = [UIColor blackColor].CGColor;//shadowColor阴影颜色
+        self.layer.shadowOffset = CGSizeMake(5,5);//shadowOffset阴影偏移,x向右偏移4，y向下偏移4，默认(0, -3),这个跟shadowRadius配合使用
+        self.layer.shadowOpacity = 0.1;//阴影透明度，默认0
+        self.layer.shadowRadius = 3;
     }
     return self;
 }
@@ -49,7 +54,7 @@
         _shapeLayer = [CAShapeLayer layer];
         _shapeLayer.fillColor = [UIColor whiteColor].CGColor;
         _shapeLayer.lineWidth=0.5;
-        _shapeLayer.strokeColor= [UIColor blackColor].CGColor;
+        _shapeLayer.strokeColor= [UIColor whiteColor].CGColor;
         _shapeLayer.path= path.CGPath;
     }
     return _shapeLayer;
